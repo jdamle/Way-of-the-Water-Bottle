b003004165b37b80eb802ccb0101eb2b107d41f1
jdamle <70299223+jdamle@users.noreply.github.com>
2023-02-07 17:38:10 -0500

    stm32 code with spi and dac
    STM32 code with working SPI for Display, can cycle through different display messages, set up DAC for outputting 1.5V for Peltier module control, set up adc

---
+++ Senior-Design-Water-Bottle/Debug/StdPeriph_Driver/src/stm32f0xx_spi.d
@@ -0,0 +1,95 @@
+StdPeriph_Driver/src/stm32f0xx_spi.o: \
+ ../StdPeriph_Driver/src/stm32f0xx_spi.c \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_spi.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/CMSIS/device/stm32f0xx.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/CMSIS/core/core_cm0.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/CMSIS/core/core_cmInstr.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/CMSIS/core/core_cmFunc.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/CMSIS/device/system_stm32f0xx.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_conf.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_adc.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_can.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_cec.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_crc.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_crs.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_comp.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_dac.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_dbgmcu.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_dma.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_exti.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_flash.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_gpio.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_syscfg.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_i2c.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_iwdg.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_pwr.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_rcc.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_rtc.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_spi.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_tim.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_usart.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_wwdg.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_misc.h \
+ C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_rcc.h
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_spi.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/CMSIS/device/stm32f0xx.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/CMSIS/core/core_cm0.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/CMSIS/core/core_cmInstr.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/CMSIS/core/core_cmFunc.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/CMSIS/device/system_stm32f0xx.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_conf.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_adc.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_can.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_cec.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_crc.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_crs.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_comp.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_dac.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_dbgmcu.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_dma.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_exti.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_flash.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_gpio.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_syscfg.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_i2c.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_iwdg.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_pwr.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_rcc.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_rtc.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_spi.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_tim.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_usart.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_wwdg.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_misc.h:
+
+C:/Users/jaide/workspace/Senior-Design-Water-Bottle/StdPeriph_Driver/inc/stm32f0xx_rcc.h:
---
+++ Senior-Design-Water-Bottle/src/main.c
@@ -0,0 +1,203 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32f0xx.h"
+//#include "string.h"
+
+const char test[] = "This bottle test";
+int sample;
+char msg;
+
+//void set_char_msg(int, char);
+void nano_wait(unsigned int);
+
+void nano_wait(unsigned int n) {
+    asm(    "        mov r0,%0\n"
+            "repeat: sub r0,#83\n"
+            "        bgt repeat\n" : : "r"(n) : "r0", "cc");
+}
+void init_spi1() {
+    // PA5  SPI1_SCK
+    // PA6  SPI1_MISO
+    // PA7  SPI1_MOSI
+    // PA15 SPI1_NSS
+
+    RCC -> AHBENR |= RCC_AHBENR_GPIOAEN;
+
+    GPIOA -> MODER &= ~(GPIO_MODER_MODER5 |
+                        GPIO_MODER_MODER6 |
+                        GPIO_MODER_MODER7 |
+                        GPIO_MODER_MODER15);
+
+    // Alternate Function 10
+    GPIOA -> MODER |= (GPIO_MODER_MODER5_1 |
+                       GPIO_MODER_MODER6_1 |
+                       GPIO_MODER_MODER7_1 |
+                       GPIO_MODER_MODER15_1);
+
+    RCC -> APB2ENR |= RCC_APB2ENR_SPI1EN;
+    SPI1 -> CR1 &= ~(SPI_CR1_SPE);
+    SPI1 -> CR1 |= SPI_CR1_BR; // 111 fpclk/256
+    SPI1 -> CR1 |= SPI_CR1_MSTR; // 1 means cfg master
+
+    // DS = 1001 10 bits
+    SPI1 -> CR2 |= SPI_CR2_DS_0 | SPI_CR2_DS_3;
+    SPI1 -> CR2 &= ~(SPI_CR2_DS_1 | SPI_CR2_DS_2);
+    SPI1 -> CR2 |= SPI_CR2_SSOE | SPI_CR2_NSSP; // have to write this register all at once to avoid possible issue with data size
+
+    SPI1 -> CR1 |= SPI_CR1_SPE;
+}
+void spi_cmd(unsigned int data) {
+    while(!(SPI1->SR & SPI_SR_TXE)) {}
+    SPI1->DR = data;
+}
+void spi_data(unsigned int data) {
+    spi_cmd(data | 0x200);
+}
+void spi1_init_oled() {
+    nano_wait(1000000);
+    spi_cmd(0x38);
+    spi_cmd(0x08);
+    spi_cmd(0x01);
+    nano_wait(2000000);
+    spi_cmd(0x06);
+    spi_cmd(0x02);
+    spi_cmd(0x0c);
+}
+void spi1_display1(const char *string) {
+    spi_cmd(0x02);
+    while(*string != '\0') {
+        spi_data(*string);
+        string++;
+    }
+}
+void spi1_display2(const char *string) {
+    spi_cmd(0xc0);
+    while(*string != '\0') {
+        spi_data(*string);
+        string++;
+    }
+}
+/*uint16_t display[34] = {
+        0x002, // Command to set the cursor at the first position line 1
+        0x200+'E', 0x200+'C', 0x200+'E', 0x200+'3', 0x200+'6', + 0x200+'2', 0x200+' ', 0x200+'i',
+        0x200+'s', 0x200+' ', 0x200+'t', 0x200+'h', + 0x200+'e', 0x200+' ', 0x200+' ', 0x200+' ',
+        0x0c0, // Command to set the cursor at the first position line 2
+        0x200+'c', 0x200+'l', 0x200+'a', 0x200+'s', 0x200+'s', + 0x200+' ', 0x200+'f', 0x200+'o',
+        0x200+'r', 0x200+' ', 0x200+'y', 0x200+'o', + 0x200+'u', 0x200+'!', 0x200+' ', 0x200+' ',
+};
+void spi1_setup_dma(void) {
+    DMA1_Channel3 -> CCR &= ~DMA_CCR_EN; // Turn off the enable bit for the channel
+
+    RCC -> AHBENR |= RCC_AHBENR_DMA1EN;
+    DMA1_Channel3 -> CPAR = (uint32_t) &(SPI1 -> DR);
+    DMA1_Channel3 -> CMAR = (uint32_t) display;
+    DMA1_Channel3 -> CNDTR = 34; // msg is 8 elements
+    DMA1_Channel3 -> CCR |= DMA_CCR_DIR; // Read from memory
+    DMA1_Channel3 -> CCR |= DMA_CCR_MINC; // Memory increment mode enabled
+    DMA1_Channel3 -> CCR &= ~(DMA_CCR_PSIZE | DMA_CCR_MSIZE); // Clearing before writing
+    DMA1_Channel3 -> CCR |= DMA_CCR_PSIZE_0 | DMA_CCR_MSIZE_0; // 01 means 16-bits
+    DMA1_Channel3 -> CCR |= DMA_CCR_CIRC;
+}*/
+void init_tim6(void) {
+    RCC->APB1ENR |= RCC_APB1ENR_TIM6EN;
+    TIM6->PSC = 48000-1;
+    TIM6->ARR = 5000-1;
+    TIM6->DIER |= TIM_DIER_UIE;
+/*    TIM6->CR2 &= ~TIM_CR2_MMS;
+    TIM6->CR2 |= TIM_CR2_MMS_1;*/
+    TIM6->CR1 |= TIM_CR1_CEN;
+    NVIC->ISER[0] = 1<<17;
+}
+void TIM6_DAC_IRQHandler(void)
+{
+    // TODO: Remember to acknowledge the interrupt right here.
+    TIM6->SR &= ~TIM_SR_UIF;
+    if(sample == 0) {
+        spi1_display1("Temperature:    ");
+        spi1_display2("20 degrees C ");
+        sample = 1;
+    }
+    else if(sample == 1) {
+        spi1_display1("Liquid Level: ");
+        spi1_display2("50 mL           ");
+        sample = 2;
+    }
+    else{
+       spi1_display1("Battery Level: ");
+       spi1_display2("50%             ");
+        sample = 0;
+    }
+/*    init_spi1();
+    spi1_init_oled();*/
+    //spi1_display1(msg);
+    //spi1_display2(test);
+
+
+
+
+
+
+}
+void init_adc(void) {
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+    GPIOA->MODER &= ~0x300;
+    GPIOA->MODER |= 0x300;
+    RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
+    RCC->CR2 |= RCC_CR2_HSI14ON;
+    while(!(RCC->CR2 & RCC_CR2_HSI14ON));
+    ADC1->CR |= ADC_CR_ADEN;
+    while(!(ADC1->ISR & ADC_ISR_ADRDY));
+    while((ADC1->CR & ADC_CR_ADSTART));
+    while (1) {
+        ADC1->CHSELR = 0;
+        ADC1->CHSELR |= 1<<4;
+        while(!(ADC1->ISR & ADC_ISR_ADRDY));
+        ADC1->CR |= ADC_CR_ADSTART;
+        while(!(ADC1->ISR & ADC_ISR_EOC));
+
+    }
+
+}
+void init_dac(void) {
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+    GPIOA->MODER &= ~0x300;
+    GPIOA->MODER |= 0x300;
+
+    RCC->APB1ENR |= RCC_APB1ENR_DACEN;
+    DAC->CR &= ~DAC_CR_EN1;
+    DAC->CR &= ~DAC_CR_BOFF1;
+    DAC->CR |= DAC_CR_TEN1;
+    DAC->CR |= DAC_CR_TSEL1;
+    DAC->CR |= DAC_CR_EN1;
+
+    int x = 2000;
+    while((DAC->SWTRIGR & DAC_SWTRIGR_SWTRIG1) == DAC_SWTRIGR_SWTRIG1);
+    DAC->DHR12R1 = x;
+    DAC->SWTRIGR |= DAC_SWTRIGR_SWTRIG1;
+}
+
+
+int main(void)
+{
+    sample = 0;
+    init_tim6();
+    init_spi1();
+    spi1_init_oled();
+    spi1_display1("Good Day! ");
+    init_dac();
+/*    init_spi1();
+    spi1_init_oled();
+    spi1_display1("Hello there,");
+    spi1_display2(test);*/
+
+
+}
